86254ab1a8f70e914b5faade7bba744f04b43fe3
chrislo27 <kinchrislo@gmail.com>
2015-07-31 10:47:10 -0700

    Initial commit

---
+++ desktop/src/ticketsplease/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ticketsplease.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ticketsplease.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
