50be6f7437381d12351c3de0cc883fea2102da4d
chrislo27 <kinchrislo@gmail.com>
2015-08-01 09:42:55 -0700

    Added background color for less dark

--- core/src/ticketsplease/renderer/Renderer.java
+++ core/src/ticketsplease/renderer/Renderer.java
@@ -6,6 +6,7 @@ import ticketsplease.registry.AssetRegistry;
 import ticketsplease.scenario.Scenario;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.Disposable;
 
@@ -22,17 +23,32 @@ public class Renderer implements Disposable {
 	}
 
 	public void render() {
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
 		batch.begin();
 		
-		batch.draw(AssetRegistry.getTexture("desk"), 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		batch.setColor(0.9f, 0.9f, 0.9f, 1);
+		Main.fillRect(batch, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		batch.setColor(1, 1, 1, 1);
 		
-		batch.flush();
-		
+		// entities being returned to the customer
 		for (Entity e : scenario.entities) {
 			if (scenario.currentDragging == e) continue;
+			if(!e.isBeingReturned) continue;
 			e.render(this);
 		}
 		
+		batch.draw(AssetRegistry.getTexture("desk"), 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		
+		// entities on the desk
+		for (Entity e : scenario.entities) {
+			if (scenario.currentDragging == e) continue;
+			if(e.isBeingReturned) continue;
+			e.render(this);
+		}
+		
+		// the current one you're dragging
 		if (scenario.currentDragging != null) {
 			scenario.currentDragging.render(this);
 		}
